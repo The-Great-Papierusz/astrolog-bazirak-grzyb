acd1ded22401f9847a1c7914e7b4a9a7bad7eb0a
Infinitzium <mikolaj.bazirak@cyfroweszkoly.pl>
2026-06-04 16:29:56 +0200

    Add spaceObj List
    List in the main App.tsx where you store all the planets

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
 //import { useState } from 'react'
 import './App.css'
+import {useState} from "react";
+import type {SpaceObject} from "./assets/SpaceObject.ts";
 
 function App() {
   // const [count, setCount] = useState(0)
+    const [spaceObj, setSpaceObj] = useState<SpaceObject[]>([
+        {
+            id: 1,
+            name: 'Mars',
+            type: 'Planet',
+            image: 'https://en.wikipedia.org/wiki/Mars#/media/File:Mars_-_August_30_2021_-_Flickr_-_Kevin_M._Gill.png',
+            distance: '225 Mil KM',
+
+        },
+        {
+            id: 2,
+            name: 'Jupiter',
+            type: 'Planet',
+            image: 'https://en.wikipedia.org/wiki/Jupiter#/media/File:Jupiter_OPAL_2024.png',
+            distance: '590 Mil KM',
+
+        }
+    ]);
 
   return (
       <>
