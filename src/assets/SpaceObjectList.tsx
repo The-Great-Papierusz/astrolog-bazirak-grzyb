56e862c57439ed00cc9a202d17c69907b0f7a97e
Infinitzium <mikolaj.bazirak@cyfroweszkoly.pl>
2026-06-06 12:08:37 +0200

    Add CelestialBodyListProps function
    this function returns a bunch of divs from a list thats given as an arguement

---
+++ src/assets/SpaceObjectList.tsx
@@ -0,0 +1,24 @@
+import type {celestialBody} from "./CelestialBody.ts";
+import {BodyView} from "./SpaceObjectView.tsx";
+
+interface CelestialBodyListProps {
+    bodyList: celestialBody[]
+}
+
+function CelestialBodyList({bodyList}: CelestialBodyListProps) {
+    return(
+        <div id='celestBodyList'>
+            {bodyList.map(
+                (body) =>
+                    (
+                        <div className='Bodies' onClick={BodyView(body)}>
+                            <img src={body.image} />
+                            <h3>{body.name}</h3>
+                        </div>
+                    )
+            )}
+        </div>
+    )
+}
+
+export default CelestialBodyList
