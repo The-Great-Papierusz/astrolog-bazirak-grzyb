3009330718a65b08510c2e646410ddb7c699d1d2
Toast-Jumpscare <pawel.grzyb@cyfroweszkoly.pl>
2026-06-12 00:55:19 +0200

    Update CelestBodyView.tsx

--- src/assets/CelestBodyView.tsx
+++ src/assets/CelestBodyView.tsx
@@ -1,9 +1,30 @@
-import type {celestialBody} from "./CelestialBody.ts";
+import type { celestialBody } from "./CelestialBody.ts";
 
-export function BodyView(body: celestialBody)
-{
-    console.log(body);
+interface Props {
+    body: celestialBody | null;
 }
-// tak ogólnie to jak niciśniesz na coś z listy to ta funkcja jest wywołana
-// zmienna body to obiekt który przechowuje dane o naciśniętej planecie (na podstawie interfaceu z CelestialBody.ts)
-// sama lista jest w głównym App.tsx i jest przechowana w UseState
+
+export default function BodyView({ body }: Props) {
+
+    if (!body) {
+        return <h2>Wybierz planetę z listy</h2>;
+    }
+
+    return (
+        <div className="bodyView">
+
+            <div className="bodyImage">
+                <img src={body.image} alt={body.name} />
+            </div>
+
+            <div className="bodyInfo">
+                <h2>{body.name}</h2>
+
+                <p>Typ: {body.type}</p>
+
+                <p>Odległość: {body.distance}</p>
+            </div>
+
+        </div>
+    );
+}
